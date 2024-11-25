289a729bd5d37e3ffca0ca91acbd0e4e57ed1c64
Rohit <rohitdhanotia2003@gmail.com>
2024-11-25 13:48:11 +0530

    Shadcn Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <div>
-      <h1 className="text-amber-500">Hello World!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
